886f0d2d9800de106769dda8ca1dcc76195ed034
Lpurobi <amipurobi@gmail.com>
2017-01-02 22:49:11 +0600

    created a php file for user

---
+++ submit_post.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compitable"
+  content="IE=edge">
+        <meta name="viewport"
+  content="width=device-width, initial-scale=1">
+          <title>Submit_post</title>
+          <link href="css/bootstrap.min.css"
+          rel="stylesheet">
+          <style>
+              h1{
+                color:blue;
+              }
+              .submit_post{
+                border:1px solid #7c73f6;
+                margin-top:50px;
+                border-radius: 15px;
+              }
+          </style>
+    </head>
+          <body>
+            <div class="container-fluid">
+              <div class="row">
+                <div class="col-sm-offset-1 col-sm-10 submit_post">
+                  <h1> Submit Post: </h1>
+            </div>
+            <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+              <script src="js/bootstrap.min.js">
+              </script>
+            </body>
+                  </html>
